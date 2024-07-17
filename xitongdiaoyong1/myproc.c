634fb03167ce489e53474053814950ec004c7866
sxj <2533044804@qq.com>
2024-07-17 19:02:08 +0800

    系统调用(getpid,getppid,fork)

--- xitongdiaoyong1/myproc.c
+++ xitongdiaoyong1/myproc.c
@@ -4,12 +4,39 @@
 
 int main()
 {
+//创建子进程  fork是一个函数  函数执行前：只有一个父进程 函数执行后：父进程+子进程
+pid_t id = fork();
+
+if(id == 0)
+{
+//子进程
  while(1)
 {
-printf("这是一个进程,我的PID是 %d,我的父进程PID是 %d\n",getpid(),getppid());
-
+printf("子进程,我的PID是 %d,我的父进程PID是 %d，ID是 %d\n",getpid(),getppid(),id);
 sleep(1);
 }
+}
+else if(id>0)
+{
+//父进程
+ while(1)
+{
+printf("父进程,我的PID是 %d,我的父进程PID是 %d,ID是 %d\n",getpid(),getppid(),id);
+sleep(2);
+}
+}
+else
+{
+//其他情况不做处理
+}
+
+// while(1)
+//{
+
+//printf("这是一个进程,我的PID是 %d,我的父进程PID是 %d\n",getpid(),getppid());
+
+//sleep(1);
+//}
 
 return 0;
 }
