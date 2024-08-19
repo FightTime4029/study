9a07df1dd3ab77119fb3e9037149bd028b90bbbe
sxj <2533044804@qq.com>
2024-08-19 16:34:08 +0800

    线程控制

--- test/mythread/mythread.cc
+++ test/mythread/mythread.cc
@@ -1,31 +1,62 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <string>
+#include <unistd.h>
 #include <cassert>
+#include <vector>
 using namespace std;
 
+class pthreadNode
+{
+    public:
+    int num;
+    pthread_t id;
+    char buffer[64];
+};
 
 void* thread_routine(void* args)
 {
-  while(true)
+    pthreadNode* pn =static_cast<pthreadNode*>(args); // 安全的强制类型转换
+    int cut = 10;
+    while(cut)
     {
-        cout<<"我是新线程!   "<< (char*)args  <<endl;
+        cout<< pn->buffer <<':'<< cut-- <<endl;
         sleep(1);
     }
+
+    //pthread_exit((void*)pn->num);  1.调用库返回
+    return (void*)pn->num; //2. 直接返回
 }
 
 
 int main()
 {
-    pthread_t tid;
-    int n = pthread_create(&tid,NULL,thread_routine,(void*)"thread one");
-    assert(n==0);
+    vector<pthreadNode *> v;
+    #define NUM 10
+  for(int i = 0; i < NUM ; i++)
+  {
+    pthreadNode* pn = new pthreadNode;
+    pn->num = i+1;
+    snprintf(pn->buffer,sizeof(pn->buffer),"%s : %d","pthread sucess",i+1);
+    int n = pthread_create(&(pn->id),NULL,thread_routine,pn);
+    v.push_back(pn);
+  }
 
-    while(true)
-    {
-        cout<<"我是主线程 ! "<<endl;
-        sleep(1);
+sleep(5);
+for(int i =0;i<v.size()/2;i++)
+{
 
-    }
+    pthread_cancel(v[i]->id);
+}
+
+  for(auto& e : v)
+  {
+    void* ret = nullptr;
+    pthread_join(e->id,&ret);
+    cout<<"已回收线程: "<<e->buffer<<"   NUM:"<<(long long)ret<<endl;
+    delete e;
+  }
+
     return 0;
 }
